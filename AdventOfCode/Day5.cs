6a6e21e8cec590be30110fa16c1024460f61b8c2
mrzepisko <rz.marcin@gmail.com>
2017-12-06 22:33:39 +0100

    day 5 part two

--- AdventOfCode/Day5.cs
+++ AdventOfCode/Day5.cs
@@ -15,9 +15,15 @@ namespace AdventOfCode {
                 Console.WriteLine(string.Format("Example data, steps: {0}", Process(example)));
             }
             Console.WriteLine("Test data, steps: {0}", Process(TestData.DATA5));
+
+
+            foreach (string example in examples) {
+                Console.WriteLine(string.Format("Example data, steps (decr): {0}", Process(example, true)));
+            }
+            Console.WriteLine("Test data, steps (decr): {0}", Process(TestData.DATA5, true));
         }
 
-        int Process(string input) {
+        int Process(string input, bool allowDecrement = false) {
             int[] commands = input.Split(' ')
                 .Select(i => int.Parse(i)).ToArray();
 
@@ -25,7 +31,15 @@ namespace AdventOfCode {
             for (int i = 0; i >= 0 && i < commands.Length;) {
                 steps++;
                 int jumpTo = commands[i] + i;
-                commands[i]++;
+                if (allowDecrement) {
+                    if (commands[i] >= 3) {
+                        commands[i]--;
+                    } else {
+                        commands[i]++;
+                    }
+                } else {
+                    commands[i]++;
+                }
                 if (jumpTo >= commands.Length || jumpTo < 0) {
                     return steps;
                 }
