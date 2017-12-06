44f6eae1248c3fcae5184308da371a0b5a921107
mrzepisko <rz.marcin@gmail.com>
2017-12-06 17:27:15 +0100

    day 1 part 2

--- AdventOfCode/Day1.cs
+++ AdventOfCode/Day1.cs
@@ -12,26 +12,45 @@ namespace AdventOfCode {
 
         public static void Run() {
             PartOne();
+            PartTwo();
         }
 
         static void PartOne() {
             Console.WriteLine("\t-- Part one --");
-            string example1 = "1122";
-            string example2 = "1111";
-            string example3 = "1234";
-            string example4 = "91212129";
+            string[] example = new string[] { "1122", "1111", "1234", "91212129", };
 
-            foreach (string input in new string[] { example1, example2, example3, example4 }) {
+            foreach (string input in example) {
                 Console.WriteLine(string.Format("\tExample data: {0}; result: {1}", input, InverseCaptcha(input)));
             }
 
             Console.WriteLine(string.Format("\n\tTest data: {0}...; result: {1}", Advent.DATA1.Substring(0, 5), InverseCaptcha(Advent.DATA1)));
         }
-        
+
         public static int InverseCaptcha(string input) {
             int result = 0;
             for (int i = 0; i < input.Length; i++) {
                 if (input[i].Equals(input[(i + 1) % input.Length])) {
+                    result += input[i] - '0'; //ascii to int
+                }
+            }
+            return result;
+        }
+
+
+        static void PartTwo() {
+            Console.WriteLine("\n\n\t-- Part two --");
+            string[] example = new string[] { "1212", "1221", "123425", "123123", "12131415", };
+
+            foreach (string input in example) {
+                Console.WriteLine(string.Format("\tExample data: {0}; result: {1}", input, InverseCaptchaPartTwo(input)));
+            }
+
+            Console.WriteLine(string.Format("\n\tTest data: {0}...; result: {1}", Advent.DATA1.Substring(0, 5), InverseCaptchaPartTwo(Advent.DATA1)));
+        }
+        public static int InverseCaptchaPartTwo(string input) {
+            int result = 0;
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i].Equals(input[(i + (input.Length / 2)) % input.Length])) {
                     result += input[i] - '0'; //ascii to int
                 }
             }
